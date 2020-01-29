fb208e2f4b8f8e3fcc12c27af5e58be45141679f
Peter McAree <me@petermcaree.com>
2020-01-29 11:40:20 +0000

    add: function to fetch chat details with some mock data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+import { fetchChatDetails, InitialMessage, ParticipantDetails } from './services/connect';
+
 const initialize = (): string => {
+    const mockParticipantDetails: ParticipantDetails = {
+        DisplayName: 'Pedro',
+    };
+    const mockInitialMessage: InitialMessage = {
+        Content: 'mock-content',
+        ContentType: 'plain/text',
+    };
+
+    const chatDetails = fetchChatDetails(
+        'mock-contact-flow-id',
+        'mock-instanceId',
+        mockParticipantDetails,
+        mockInitialMessage,
+    );
+
     return 'Successfully hitting the function within the library!';
 };
 
--- src/services/connect.ts
+++ src/services/connect.ts
@@ -1,5 +1,14 @@
 import { Connect } from 'aws-sdk';
 
+export interface ParticipantDetails {
+    DisplayName: string;
+}
+
+export interface InitialMessage {
+    Content: string;
+    ContentType: string;
+}
+
 interface ChatDetails {
     ContactId: string;
     ParticipantId: string;
@@ -11,27 +20,36 @@ const initializeConnect = (): Connect => {
     return new Connect();
 };
 
-const fetchChatDetails = (): ChatDetails => {
-    // TODO: Utilise Connect instance to invoke .startChatConnect() in order to fetch ChatDetails
+/**
+ * Establish a connection, fetch the details required to send messages
+ * @param contactFlowId The identifier of the contact flow for the chat
+ * @param instanceId The identifier of the Amazon Connect instance
+ * @param participantDetails Information identifying the participant
+ * @param initialMessage The initial message to be sent to the newly created chat
+ */
+const fetchChatDetails = (
+    contactFlowId: string,
+    instanceId: string,
+    participantDetails: ParticipantDetails,
+    initialMessage: InitialMessage,
+): ChatDetails | null => {
+    let chatDetails = null;
     const connectInstance = initializeConnect();
 
-    // const params = {
-    //     ContactFlowId: 'STRING_VALUE' /* required */,
-    //     InstanceId: 'STRING_VALUE' /* required */,
-    //     ParticipantDetails: {
-    //         /* required */
-    //         DisplayName: 'STRING_VALUE' /* required */,
-    //     },
-    //     InitialMessage: {
-    //         Content: 'STRING_VALUE' /* required */,
-    //         ContentType: 'STRING_VALUE' /* required */,
-    //     },
-    // };
-    return {
-        ContactId: 'mock-contact-id',
-        ParticipantId: 'mock-participant-id',
-        ParticipantToken: 'mock-participant-token',
+    const params = {
+        ContactFlowId: contactFlowId,
+        InstanceId: instanceId,
+        ParticipantDetails: participantDetails,
+        InitialMessage: initialMessage,
     };
+
+    connectInstance.startChatContact(params, (err, data) => {
+        if (err) throw new Error(err.stack);
+
+        chatDetails = data;
+    });
+
+    return chatDetails;
 };
 
 export { fetchChatDetails };
